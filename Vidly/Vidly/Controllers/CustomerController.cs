0457d99c4f53bbe7125be2001bb5a81bc2b5de30
Swikar Bhattarai <CA20417@tn.gov>
2019-04-17 11:08:18 -0500

    Added Models, Controller and Index page for Movies and Customers

---
+++ Vidly/Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customer = new List<Customer>
+            {
+               new Customer { Id = 1, Name= "Customer1"},
+               new Customer { Id = 2, Name= "Customer2"},
+               new Customer { Id = 3, Name= "Customer3"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Customers = customer
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new List<Movie>
+            {
+                new Movie{ Id = 1, Name = "Shrek!"},
+                new Movie{ Id = 2, Name = "Batman"},
+                new Movie{ Id = 3, Name = "Superman"},
+                new Movie{ Id = 4, Name = "Spiderman"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                 Movies = movie
+            };
+            return View(viewModel);
+        }
+
+    }
+}
